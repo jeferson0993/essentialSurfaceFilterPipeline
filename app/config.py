79e78eba93e6ab843ad1ca05a89d5e35ac832cd3
Jeferson F Silva <164197493+jeferson0993@users.noreply.github.com>
2026-02-17 16:43:05 -0300

    Add configuration settings for input and results

---
+++ app/config.py
@@ -0,0 +1,15 @@
+INPUT_FASTA = "data/ecoli_nonredundant.fasta"
+
+DEG_URL = "http://tubic.org/deg/public/download/DEG10.aa.gz"
+DEG_FASTA = "data/DEG10.aa"
+
+EVALUE_THRESHOLD = 1e-5
+
+RESULTS_DIR = "results"
+
+SURFACE_LOCATIONS = [
+    "OuterMembrane",
+    "Extracellular",
+    "Periplasmic",
+    "CytoplasmicMembrane"
+]
